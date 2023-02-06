19810ffcef73d0d9f1a90d81034d00b892310060
undefined <sjx233@qq.com>
2023-02-06 15:07:37 +0800

    show a proper error message on aarch64

--- src/main/java/ud2/syphonizer/Syphonizer.java
+++ src/main/java/ud2/syphonizer/Syphonizer.java
@@ -1,33 +1,41 @@
 package ud2.syphonizer;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
-import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
 public class Syphonizer implements PreLaunchEntrypoint {
+  public static final Logger LOGGER = LoggerFactory.getLogger("syphonizer");
+
   @Override
   public void onPreLaunch() {
+    var arch = System.getProperty("os.arch");
     try {
-      addLibrary("libJSyphon.jnilib", "Syphon");
+      var root = Path.of(getProperty("java.library.path"));
+      Files.createDirectories(root);
+      for (var filename : new String[] { "libJSyphon.jnilib", "Syphon" }) {
+        var path = root.resolve(filename);
+        LOGGER.debug("copying to {}", path);
+        try (var in = Syphonizer.class.getResourceAsStream("/natives/" + arch + "/" + filename)) {
+          if (in == null)
+            throw new RuntimeException(filename + " is not available for " + arch);
+          Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
+        }
+      }
     } catch (IOException e) {
-      throw new RuntimeException("Failed to load natives.", e);
+      throw new RuntimeException("Failed to load natives", e);
     }
   }
 
-  private static void addLibrary(String... filenames) throws IOException {
-    var root = Path.of(StringUtils.split(System.getProperty("java.library.path"), File.pathSeparatorChar)[0]);
-    Files.createDirectories(root);
-    for (var filename : filenames) {
-      var path = root.resolve(filename);
-      try (var in = Syphonizer.class.getResourceAsStream("/natives/" + filename)) {
-        Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
-      }
-    }
+  private static String getProperty(String key) {
+    var value = System.getProperty(key);
+    LOGGER.debug("{} = {}", key, value);
+    return value;
   }
 }
